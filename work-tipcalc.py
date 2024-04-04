df97134ee267de6394fffe4e0a4662b509897fd4
amandaarenn <165818899+amandaarenn@users.noreply.github.com>
2024-04-04 11:24:27 -0700

    work-tipcalc.py

--- work-tipcalc.py
+++ work-tipcalc.py
@@ -8,10 +8,12 @@ def main();
     tip = dollars * percent
   # Note: This is one way to round a number to two decimal places
     print(f"Leave ${round(tip, 2)}")
+
 def dollars_to_float(d: str):
     # Converts string dollars to a decimal float
     #    Returns the result
  return float(d)
+
 def percent_to_float(p):
     # Converts percent to a decimal float
     #    Returns the result
