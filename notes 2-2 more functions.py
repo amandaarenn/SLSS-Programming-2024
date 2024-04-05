0d5ecd5fbbd1258317e510a1039628bdaa4d9e59
Amanda Ren <amandaren37@gmail.com>
2024-04-05 13:55:39 -0700

    fixed problems in more functions

--- notes 2-2 more functions.py
+++ notes 2-2 more functions.py
@@ -4,16 +4,19 @@
 
 def stars (num_stars: int) -> str:
     """Returns a given number of *"""
-   value = ""
+    value = ""
    
-   # If the value is == 0 or value ==1 "*"
-# if value > 1 "*" * num_stars
-# else negative number -> error
-if num_stars == 0 or num_stars == 1:
-    value = "*"
-elif num_stars > 1
-  
-    return: "*" * num_stars
+    # if the value is == 0 or value ==1 "*"
+    # if value > 1 "*" * num_stars
+    # else negative number -> error
+    if num_stars == 0 or num_stars == 1:
+        value = "*"
+    elif num_stars > 1:
+        value = "*" * num_stars
+    else:
+        value = "Sorry, can't take negative numbers."
+
+    return value
 
 
 # Multiply Strings
@@ -21,8 +24,7 @@ elif num_stars > 1
 # print(greeting * 2)
 
 # print("The quick brown fox jumps over the lazy dog." * 2)
-
-print (stars(1)) # "*"
-print (stars(100))
-print (stars(0)) # "
-print (stars(-1))
+print(stars(1))  # "*"
+print(stars(100))  # "*" * 100
+print(stars(0))
+print(stars(-1))
